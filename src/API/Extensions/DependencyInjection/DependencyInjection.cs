79597fe3f27be4e7a2c0265bb24e84aca884855b
Andras Rajtar <rajtarandras@gmail.com>
2026-02-24 22:56:16 +0100

    Init 2

---
+++ src/API/Extensions/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,9 @@
+namespace API.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static WebApplicationBuilder AddServices(this WebApplicationBuilder builder)
+    {
+        return builder;
+    }
+}
---
+++ tests/API.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace API.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void TrueTest()
+    {
+        Assert.True(true);
+    }
+}
